a104b896930857ccd8c08f3210298f15b3a532bb
sakshi-2306-yz <autisakshi3@gmail.com>
2026-08-20 13:23:52 +0530

    Implement newsletter email queue templating

--- app/Models/Tag.php
+++ app/Models/Tag.php
@@ -4,19 +4,30 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Tag extends Model
 {
     use SoftDeletes;
+
     public function contactTags(): HasMany
     {
         return $this->hasMany(ContactTag::class);
     }
 
-     public function newsletters(): HasMany
+    public function newsletters(): HasMany
     {
         return $this->hasMany(Newsletter::class);
     }
 
+    public function contacts(): BelongsToMany
+    {
+        return $this->belongsToMany(
+            Contact::class,
+            'contact_tags',
+            'tag_id',
+            'contact_id'
+        );
+    }
 }
